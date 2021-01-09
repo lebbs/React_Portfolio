f82954fa094534d9d533549704392791a407a35f
lebbis <44391814+lebbis@users.noreply.github.com>
2021-01-09 20:18:51 +0200

    api added

---
+++ portfolio-api/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sendGrid = require('@sendgrid/mail');
+const app = express();
+require('dotenv').config();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Acces-Control-Allow-Origin', '*');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Acces-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get('/api', (req, res, next) => {
+    res.send('API status: Running')
+});
+
+app.post('/api/email', (req,res,next) => {
+
+    sendGrid.setApiKey(process.env.API_KEY);
+    const msg = {
+        to: 'leppanen.joonas63@gmail.com',
+        from: req.body.email,
+        subject: 'Website Contact',
+        text: req.body.message
+    }
+
+    sendGrid.send(msg)
+        .then(result => {
+            res.status(200).json({
+                succes: true
+            });    
+
+        })
+        .catch(err =>{
+
+            console.log('error: ',err)
+            res.status(401).json({
+                succes: false
+            });
+
+        });
+
+});
+
+app.listen(3030, '0.0.0.0');
